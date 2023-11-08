c9eb30681504137ca843edad962a2ea3d0ab6360
Khaled Samir <66645843+khaledsamirr@users.noreply.github.com>
2023-11-08 17:16:10 +0200

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -54,15 +54,7 @@ app.use("/api/messages",messageRoute);
 
 
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept'
-    );
-    next();
-});
+
 
 app.listen(8800,()=>{
     connect()
